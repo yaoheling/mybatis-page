1c481831fde1e515fcee4de38da10cef35ccc3fd
lvhongye <ufowwe@163.com>
2018-06-08 14:59:04 +0800

    添加插件，每次生成时覆盖xml

---
+++ src/main/java/com/mcipay/plugin/OverwriteIsMergeablePlugin.java
@@ -0,0 +1,33 @@
+package com.mcipay.plugin;
+
+import org.mybatis.generator.api.GeneratedXmlFile;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * @author hongye.lv
+ * @date 2018/06/08
+ **/
+public class OverwriteIsMergeablePlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapGenerated(GeneratedXmlFile sqlMap, IntrospectedTable introspectedTable) {
+        try {
+            Field field = sqlMap.getClass().getDeclaredField("isMergeable");
+            field.setAccessible(true);
+            field.setBoolean(sqlMap, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+}
